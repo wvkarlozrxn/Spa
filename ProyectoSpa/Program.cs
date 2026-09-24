ff39e4a11c7b7662f718cf72edd51a9663025dcc
herrerajuanjose825-sys <herrerajuanjose825@.com>
2026-09-23 21:28:32 -0500

    Creacion de aplicacion de estandares y entidades

--- ProyectoSpa/Program.cs
+++ ProyectoSpa/Program.cs
@@ -5,7 +5,7 @@ public class Cargos
     public int Id_Cargo { get; set; }
     public string? Cargo { get; set; }
 
-    public List<Empleados> Empleados { get; set; }
+    public List<Empleados>? Empleados { get; set; }
 }
 
 public class Inventarios
@@ -14,7 +14,7 @@ public class Inventarios
     public int Cantidad { get; set; }
     public int Stock_min { get; set; }
 
-    public List<Productos> Productos { get; set; }
+    public List<Productos>?  Productos { get; set; }
 }
 
 public class Proveedores
@@ -25,7 +25,7 @@ public class Proveedores
     public string? Correo { get; set; }
     public string? Direccion { get; set; }
 
-    public List<Compras> Compras { get; set; }
+    public List<Compras>? Compras { get; set; }
 }
 
 public class Compras
@@ -37,9 +37,9 @@ public class Compras
     public int Estado { get; set; }
     public int Id_Proveedor { get; set; }
 
-    public Proveedores _Proveedor { get; set; }
+    public Proveedores? _Proveedor { get; set; }
 
-    public List<Detalle_Compras> Detalle_Compras { get; set; }
+    public List<Detalle_Compras>? Detalle_Compras { get; set; }
 }
 
 public class Turnos
@@ -49,7 +49,7 @@ public class Turnos
     public DateTime Hora_Entrada { get; set; }
     public DateTime Hora_Salida { get; set; }
 
-    public List<Empleados_Turnos> Empleados_Turnos { get; set; }
+    public List<Empleados_Turnos>? Empleados_Turnos { get; set; }
 }
 
 public class Detalle_Facturas
@@ -61,8 +61,8 @@ public class Detalle_Facturas
     public decimal Precio_Unidad { get; set; }
     public decimal Subtotal { get; set; }
 
-    public Facturas _Factura { get; set; }
-    public Servicios _Servicio { get; set; }
+    public Facturas? _Factura { get; set; }
+    public Servicios? _Servicio { get; set; }
 }
 
 public class Facturas
@@ -74,10 +74,10 @@ public class Facturas
     public decimal Total { get; set; }
     public int Id_Reserva { get; set; }
 
-    public Reservas _Reserva { get; set; }
+    public Reservas? _Reserva { get; set; }
 
-    public List<Detalle_Facturas> Detalle_Facturas { get; set; }
-    public List<Pagos> Pagos { get; set; }
+    public List<Detalle_Facturas>? Detalle_Facturas { get; set; }
+    public List<Pagos>? Pagos { get; set; }
 }
 
 public class Servicios
@@ -88,8 +88,8 @@ public class Servicios
     public DateTime Duracion { get; set; }
     public decimal Precio { get; set; }
 
-    public List<Detalle_Reservas> Detalle_Reservas { get; set; }
-    public List<Detalle_Facturas> Detalle_Facturas { get; set; }
+    public List<Detalle_Reservas>? Detalle_Reservas { get; set; }
+    public List<Detalle_Facturas>? Detalle_Facturas { get; set; }
 }
 
 public class Reservas
@@ -101,11 +101,11 @@ public class Reservas
     public int Id_Empleado { get; set; }
     public int Id_Cliente { get; set; }
 
-    public Empleados _Empleado { get; set; }
-    public Clientes _Cliente { get; set; }
+    public Empleados? _Empleado { get; set; }
+    public Clientes? _Cliente { get; set; }
 
-    public List<Facturas> Facturas { get; set; }
-    public List<Detalle_Reservas> Detalle_Reservas { get; set; }
+    public List<Facturas>? Facturas { get; set; }
+    public List<Detalle_Reservas>? Detalle_Reservas { get; set; }
 }
 
 public class Detalle_Compras
@@ -117,8 +117,8 @@ public class Detalle_Compras
     public int Id_Compra { get; set; }
     public int Id_Producto { get; set; }
 
-    public Compras _Compra { get; set; }
-    public Productos _Producto { get; set; }
+    public Compras? _Compra { get; set; }
+    public Productos? _Producto { get; set; }
 }
 
 public class Productos
@@ -130,9 +130,9 @@ public class Productos
     public decimal Precio_Venta { get; set; }
     public int Id_Inventario { get; set; }
 
-    public Inventarios _Inventario { get; set; }
+    public Inventarios? _Inventario { get; set; }
 
-    public List<Detalle_Compras> Detalle_Compras { get; set; }
+    public List<Detalle_Compras>? Detalle_Compras { get; set; }
 }
 
 public class Detalle_Reservas
@@ -143,8 +143,8 @@ public class Detalle_Reservas
     public int Id_Reserva { get; set; }
     public int Id_Servicio { get; set; }
 
-    public Reservas _Reserva { get; set; }
-    public Servicios _Servicio { get; set; }
+    public Reservas? _Reserva { get; set; }
+    public Servicios? _Servicio { get; set; }
 }
 
 public class Pagos
@@ -153,9 +153,9 @@ public class Pagos
     public DateTime Fecha_Pago { get; set; }
     public int Id_Factura { get; set; }
 
-    public Facturas _Factura { get; set; }
+    public Facturas? _Factura { get; set; }
 
-    public List<Metodos_Pagos> Metodos_Pagos { get; set; }
+    public List<Metodos_Pagos>? Metodos_Pagos { get; set; }
 }
 
 public class Metodos_Pagos
@@ -165,7 +165,7 @@ public class Metodos_Pagos
     public string? Metodo_Pago { get; set; }
     public int Id_Pago { get; set; }
 
-    public Pagos _Pago { get; set; }
+    public Pagos? _Pago { get; set; }
 }
 
 public class Clientes
@@ -173,9 +173,9 @@ public class Clientes
     public int Id_Cliente { get; set; }
     public int Id_Persona { get; set; }
 
-    public Personas _Persona { get; set; }
+    public Personas? _Persona { get; set; }
 
-    public List<Reservas> Reservas { get; set; }
+    public List<Reservas>? Reservas { get; set; }
 }
 
 public class Empleados
@@ -185,11 +185,11 @@ public class Empleados
     public int Id_Cargo { get; set; }
     public DateTime Fecha_Contrato { get; set; }
 
-    public Cargos _Cargo { get; set; }
-    public Personas _Persona { get; set; }
+    public Cargos? _Cargo { get; set; }
+    public Personas? _Persona { get; set; }
 
-    public List<Reservas> Reservas { get; set; }
-    public List<Empleados_Turnos> Empleados_Turnos { get; set; }
+    public List<Reservas>? Reservas { get; set; }
+    public List<Empleados_Turnos>? Empleados_Turnos { get; set; }
 }
 
 public class Empleados_Turnos
@@ -198,8 +198,8 @@ public class Empleados_Turnos
     public int Id_Turno { get; set; }
     public int Id_Empleado { get; set; }
 
-    public Turnos _Turno { get; set; }
-    public Empleados _Empleado { get; set; }
+    public Turnos? _Turno { get; set; }
+    public Empleados? _Empleado { get; set; }
 }
 
 public class Personas
@@ -209,8 +209,8 @@ public class Personas
     public string? Telefono { get; set; }
     public string? Cedula { get; set; }
 
-    public List<Clientes> Clientes { get; set; }
-    public List<Empleados> Empleados { get; set; }
+    public List<Clientes>? Clientes { get; set; }
+    public List<Empleados>? Empleados { get; set; }
 }
 
 
--- ProyectoSpa/obj/Release/net10.0/ProyectoSpa.AssemblyInfo.cs
+++ ProyectoSpa/obj/Release/net10.0/ProyectoSpa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSpa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27cc603f52c4c84a90e8205c56c2de4a91f95a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5603039cf0143c12ee0dcd396d70ef6c7b151674")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSpa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSpa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
